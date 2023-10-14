58a595d470d8e73018eb5b7bb9f36742ac9449e0
Sagar Ghodke <134285837+sagarghodke1204@users.noreply.github.com>
2023-10-14 16:38:18 +0530

    Create sagar.java

---
+++ Java/sagar.java
@@ -0,0 +1,7 @@
+public class My_name {
+
+    public static void main(String[] args) {
+        System.out.println("My name is sagar, I am Contributing in Hacktober Open Source Project");
+    }
+    
+}
